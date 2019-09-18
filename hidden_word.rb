f346a861546acf18cca372864855e39257cc8bd3
Laurie Frank <laurie360@hotmail.co.uk>
2019-09-18 15:01:39 +0100

    incomplete code - Harrison has provided notes for alternative (i.e. an actual solution)

--- hidden_word.rb
+++ hidden_word.rb
@@ -4,9 +4,47 @@ class HiddenWord
 
   def initialize(word)
     @word = word
+    @word_array = []
+    @guesses
   end
 
-  def split_word(word)
-    word.split("")
+  def split_word()
+    split_word = @word.split("")
+    @word_array = split_word
+    p @word_array
+    return @word_array
   end
+
+  def does_word_include_letter(guess)
+    p @word_array
+    for letter in @word_array
+      if letter.include?(guess)
+        return true
+      end
+    end
+  end
+
+  def display_true_characters(guess_result)
+    # @word_array.does_word_include_letter(guess_result)
+    # for letter in @word_array
+    #   if guess_result == true
+    #     return guess_result
+    # #   end
+    # end
+    #   else
+    #     letter == "*"
+    return @word_array.tr("^ ", "*") if @word.include?(guess_result) == false
+
+  end
+
 end
+
+
+# potential way to solve problem
+# just store hidden word
+# also store an array of every guess so far
+# every time you must display word:
+#    loop through words letters as array (.split)
+#    if letter is included in array of guessed letters, add letter to new 'shown_word' array
+#    else add * to new 'shown_word' array
+# return shown_word array
--- specs/hidden_word_spec.rb
+++ specs/hidden_word_spec.rb
@@ -8,11 +8,28 @@ class TestHiddenWord < MiniTest::Test
     @hidden_word = HiddenWord.new("melting")
   end
 
-  def test_hidden_word_contents
+  def test_hidden_word_contents()
     assert_equal("melting", @hidden_word.word)
   end
 
-  def test_will_split_word
+  def test_will_split_word()
     assert_equal(["m", "e", "l", "t", "i", "n", "g"], @hidden_word.split_word(@hidden_word.word))
   end
+
+  def test_is_letter_in_word()
+    @hidden_word.split_word(@hidden_word.word)
+    result = @hidden_word.does_word_include_letter("m")
+    assert_equal(true, result)
+  end
+
+  def test_display_true_characters()
+    split_word = @hidden_word.split_word(@hidden_word.word)
+    # guess_result = @hidden_word.does_word_include_letter("m")
+    actual_result = @hidden_word.display_true_characters(guess_result)
+    assert_equal("m******", actual_result)
+  end
+
 end
+
+
+#gsub
