6b0e9a62fffb6843287dbc3e0ade3c242532145d
Laurie Frank <laurie360@hotmail.co.uk>
2019-09-18 11:58:33 +0100

    initial file set up complete...in loose terms

--- specs/game_spec.rb
+++ specs/game_spec.rb
@@ -7,7 +7,18 @@ require_relative("../player.rb")
 class TestGame < MiniTest::Test
 
   def setup()
+    @player = Player.new("Laurike", 6)
+    @hidden_word = HiddenWord.new("melting")
+    @game = Game.new(@player.name, @hidden_word.word)
 
+  end
+
+  def test_player_name
+    assert_equal("Laurike", @game.player)
+  end
+
+  def test_hidden_word_contents
+    assert_equal("melting", @game.hidden_word)
   end
 
 end
